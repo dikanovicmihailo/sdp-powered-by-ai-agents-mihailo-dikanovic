00c3bc01b0acfb2f6667be265ebd5b0d9cb27520
Mihailo Dikanovic <mihailo.dikanovic@rt-rk.com>
2026-04-19 21:57:53 +0200

    #20 feat(commands): implement NAV-BE-001.1-S1 TurnLeft changes heading

---
+++ mars_rover/domain/commands.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mars_rover.domain.rover import Rover
+
+
+class TurnLeft:
+    def __call__(self, rover: Rover) -> None:
+        rover.heading = rover.heading.turn_left()
