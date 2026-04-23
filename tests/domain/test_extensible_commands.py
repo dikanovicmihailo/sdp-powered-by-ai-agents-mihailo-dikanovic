6c22acb174e71aea50ab86d8ad83b84478de67a7
Mihailo Dikanovic <mihailo.dikanovic@rt-rk.com>
2026-04-23 19:05:07 +0200

    #35 feat(domain): implement NAV-BE-004.1-S1 UTurn from East faces West

--- tests/domain/test_extensible_commands.py
+++ tests/domain/test_extensible_commands.py
@@ -17,3 +17,13 @@ class TestExtensibleCommands(unittest.TestCase):
 
         # THEN: Rover faces South
         self.assertEqual(rover.heading, Heading.S)
+
+    def test_nav_be_004_s1_given_rover_facing_east_when_uturn_then_faces_west(self):
+        # GIVEN: Rover facing East
+        rover = Rover(0, 0, Heading.E)
+
+        # WHEN: UTurn is executed
+        UTurn()(rover)
+
+        # THEN: Rover faces West
+        self.assertEqual(rover.heading, Heading.W)
