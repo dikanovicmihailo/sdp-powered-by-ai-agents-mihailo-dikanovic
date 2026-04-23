a7e8fdb7495cc325304c48194f27148dc3c4d2bf
Mihailo Dikanovic <mihailo.dikanovic@rt-rk.com>
2026-04-23 19:04:42 +0200

    #35 feat(domain): implement NAV-STORY-004-S1 UTurn rotates rover 180 degrees

---
+++ tests/domain/test_extensible_commands.py
@@ -0,0 +1,19 @@
+import unittest
+
+from mars_rover.domain.commands import UTurn
+from mars_rover.domain.heading import Heading
+from mars_rover.domain.rover import Rover
+
+
+class TestExtensibleCommands(unittest.TestCase):
+    def test_nav_story_004_s1_given_rover_facing_north_when_uturn_then_faces_south(
+        self,
+    ):
+        # GIVEN: Rover facing North
+        rover = Rover(0, 0, Heading.N)
+
+        # WHEN: UTurn is executed
+        UTurn()(rover)
+
+        # THEN: Rover faces South
+        self.assertEqual(rover.heading, Heading.S)
